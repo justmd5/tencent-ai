09b134304b6208219df0c578e6d219d60b51bf60
justmd5 <heyjun2012@gmail.com>
2018-06-24 21:43:56 +0800

    fix

--- src/Providers/FaceServiceProvider.php
+++ src/Providers/FaceServiceProvider.php
@@ -22,15 +22,6 @@ class FaceServiceProvider implements ServiceProviderInterface
      *
      * @param Container $pimple A container instance
      */
-
-    /**
-     * Registers services on the given container.
-     *
-     * This method should only be used to configure services and parameters.
-     * It should not get services.
-     *
-     * @param Container $pimple A container instance
-     */
     public function register(Container $pimple)
     {
         $filter = [
